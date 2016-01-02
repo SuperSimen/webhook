e0e46a21d825e1bd451a84398f24a50a07100f75
Simen Owesen-Lein <simen.owesenlein@gmail.com>
2016-01-02 21:44:02 +0100

    Working

--- Main.hs
+++ Main.hs
@@ -6,42 +6,29 @@ import Control.Monad.IO.Class
 import qualified Data.ByteString.Lazy.Char8 as B
 import System.Process
 import qualified Data.Text.Lazy as T
+import Data.List
 
-hello _ respond = do
-    putStrLn "I've done some IO here"
-    respond $ responseLBS
-        status200
-        [("Content-Type", "text/plain")]
-        "Hello, Web!"
+app request respond = case (requestMethod request, rawPathInfo request, getIpFromFullAdress $ show $ remoteHost request) of
+    ("GET", "/webhook", "127.0.0.1")    -> goGit request respond
+    _                                    -> goAway request respond
 
+getIpFromFullAdress (':':xs) = []
+getIpFromFullAdress (x:xs) = x : getIpFromFullAdress xs
 
-
-app3 :: Application
-app3 request respond = case rawPathInfo request of
-    _       -> onlyPost request respond
-
-onlyPost request respond = case (requestMethod request, rawPathInfo request, show $ remoteHost request) of
-    ("POST", "/BOI", '1':'2':'7':'.':'0':'.':'0':'.':'1':x)  -> blast request respond
-    _       -> goAway request respond
-
-
-        
-blast request respond = do
+goGit request respond = do
     putStrLn $ show $ remoteHost request
-    beam <- liftIO (readProcess "echo" ["oh yeah"] "")
-    respond $ responseLBS
-        status200
-        [("Content-Type", "text/plain")]
-        $ do
-            mconcat [B.pack beam, "yeah", B.pack $ show $ requestMethod request]
+    spawnCommand "git -C /home/simen/src/test.no pull"
+    respond $ makePage "Fine!"
 
-goAway request respond = respond $ responseLBS
+goAway request respond = respond $ makePage "GO AWAY!";
+
+makePage content = responseLBS
     status200
     [("Content-Type", "text/plain")]
-    "GO AWAY"
-
+    content
+    
 
 main :: IO ()
 main = do
     putStrLn $ "http://localhost:8080/"
-    run 8080 app3
+    run 8080 app
